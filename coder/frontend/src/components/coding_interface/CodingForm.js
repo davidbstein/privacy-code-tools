d3e106d9552de8ab114fadb3a404a900afb4cf58
David Stein <davidbstein@gmail.com>
2020-06-18 15:41:06 -0400

    merge tool improvements

--- coder/frontend/src/components/coding_interface/CodingForm.js
+++ coder/frontend/src/components/coding_interface/CodingForm.js
@@ -24,6 +24,15 @@ const _sentenceCount = (sentences_by_doc) => {
   return _.sum(_.values(sentences_by_doc).map(e=>_.sum(_.values(e).map(ee=>ee.length))));
 }
 
+const _stringifySentences = (sentences) => {
+  const to_ret = []
+  for (var doc in sentences) {
+    for (var p in sentences[doc]) {
+      to_ret.push(`${doc}❡${p}(${sentences[doc][p].join(',')})`)
+    }
+  }
+  return to_ret;
+}
 
 class MergeItem extends Component {
   render() {
@@ -32,10 +41,13 @@ class MergeItem extends Component {
       (v,k) => v?k:undefined
       ).filter(e=>e);
     return <div>
-      {this.props.fmw_answer?<div className="merge-tool-response-header"><b>"FMW's response"</b></div>:""}
+      {this.props.fmw_answer?<div className="merge-tool-response-header"><b>FMW's response</b></div>:""}
       <div className={"merge-tool-response" + (this.props.fmw_answer?" fmw-answer": "")}>
+        <div className="merge-tool-coder-email">
+          {this.props.author}
+        </div>
         <div className="merge-tool-response-values">
-          {selectedValues.map(value=> <span>{value}</span>)}
+          {selectedValues.map((value, i)=> <span key={i}>{value}</span>)}
         </div>
         <div className="merge-tool-sentence-count">
           {_sentenceCount(this.props.sentences)} sentences flagged
@@ -45,6 +57,9 @@ class MergeItem extends Component {
         </div>
         <div className="merge-tool-comment">
           {this.props.comment ? this.props.comment : ""}
+        </div>
+        <div className="merge-tool-sentences">
+          {this.props.sentences.map((s, i) => <div key={i}> {s} </div>)}
         </div>
       </div>
     </div>
@@ -57,12 +72,12 @@ const MergeTool = connect(
 )(
   class MergeTool extends Component {
     render () {
-      const responses = []
-      const authors = []
-      for (var ci of _.values(this.props.model.coding_instances)){
-        responses.push(ci.coding_values[this.props.question_idx]);
-        authors.push(ci.coder_email);
-      }
+      const responses = this.props.mergeData.responses;
+      const sentence_strings = _.map(responses, (r) => {
+        return _stringifySentences(r.sentences);
+      });
+      const agreed_sentences = _.intersection(...sentence_strings)
+      console.log(sentence_strings, agreed_sentences);
       return <div className="merge-tool-response-list">
         {responses.map((vals, idx_) => {
           if (!vals) return;
@@ -71,14 +86,61 @@ const MergeTool = connect(
             values={vals.values}
             confidence={vals.confidence}
             comment={vals.comment}
-            sentences={vals.sentences}
-            fmw_answer={authors[idx_]=='florencia.m.wurgler@gmail.com'}
+            sentences={_.difference(sentence_strings[idx_], agreed_sentences)}
+            author={this.props.mergeData.authors[idx_]}
+            fmw_answer={this.props.mergeData.authors[idx_]=='florencia.m.wurgler@gmail.com'}
           />
         })}
+        <div className='merge-tool-agreed-sentences'>
+          Sentences highlighted by everyone:
+          {agreed_sentences.map((s, i) => <div key={i}> {s} </div>)}
+        </div>
       </div>
     }
   }
 );
+
+class MergeBoxHeader extends Component {
+  render() {
+    const responses = this.props.mergeData.responses;
+    const answers_match = _.uniqBy(_.map(responses, (r) => {
+      return _.filter(_.keys(r.values), (k) => {
+          return r.values[k];
+        })
+      }),
+      u=>u.join("--"))
+      .length == 1;
+    const sentence_strings = _.map(responses, (r) => {
+      return _stringifySentences(r.sentences);
+    })
+    const sentences_match = _.uniqBy(sentence_strings, ss => ss.join('--')).length == 1;
+    return <div>
+      <div className={`merge-header-answers-match ${(answers_match ?
+        'matching-answer-merge':'unmatching-answer-merge')}`}>
+        {answers_match ? `answers match` : `answers do not match`}
+      </div>
+      <div className={`merge-header-sentence-overlap ${(sentences_match ?
+        'matching-sentences' : 'unmatching-sentences')}`}>
+        {sentences_match ? `sentences match` : `sentences do not match`}
+      </div>
+      <div> {this.props.mergeData.authors.join(', ')}</div>
+    </div>
+  }
+}
+
+class QuestionBoxHeader extends Component {
+  render() {
+    return <div>
+      {this.props.number_of_sentences} sentence{this.props.number_of_sentences == 1 ? "" : "s"} marked relevant.
+      <br/>
+      coding: {
+        this.props.value_strings.length > 0 ?
+        this.props.value_strings.map((s, i) => <span key={i} className='coding-form-response'>{s}</span>) :
+        <span className='coding-form-uncoded-marker'>(blank)</span>
+      }
+    </div>
+  }
+}
 
 const QuestionBox = connect(
   mapStateToProps,
@@ -94,7 +156,23 @@ const QuestionBox = connect(
       this.props.userSelectQuestion(this.props.idx);
     }
 
+    getMergeData() {
+      const to_ret = {responses: [], authors: []}
+      for (var ci of _.values(this.props.model.coding_instances)){
+        if (ci.coding_values[this.props.idx]){
+          to_ret.responses.push(ci.coding_values[this.props.idx]);
+          to_ret.authors.push(ci.coder_email);
+        }
+      }
+      const sentences_by_coder = {}
+      for (var ci of _.values(this.props.model.coding_instances)){
+        const sentences = ((ci.coding_values[this.props.idx] || {}).sentences || {})[this.props.policy_type];
+      }
+      return to_ret
+    }
+
     render() {
+      const mergeData = this.getMergeData()
       const sentences = ((this.props.localState.localCoding[this.props.idx]||{}).sentences||{});
       const number_of_sentences = _sentenceCount(sentences);
       const is_active = this.props.idx == this.props.localState.selectedQuestion;
@@ -110,7 +188,7 @@ const QuestionBox = connect(
               {this.props.content.details || ""}
             </div>
             { this.props.localState.merge_mode ?
-              <MergeTool question_idx={this.props.idx} /> : <div /> }
+              <MergeTool question_idx={this.props.idx} mergeData={mergeData} /> : <div /> }
             <QuestionValueSelector
               values={this.props.content.values}
               question_idx={this.props.idx}
@@ -127,13 +205,9 @@ const QuestionBox = connect(
             {this.props.idx+1}. {this.props.content.question}
           </div>
           <div className="coding-form-question-sentence-count">
-            {number_of_sentences} sentence{number_of_sentences == 1 ? "" : "s"} marked relevant.
-            <br/>
-            coding: {
-              value_strings.length > 0 ?
-              value_strings.map((s, i) => <span key={i} className='coding-form-response'>{s}</span>) :
-              <span className='coding-form-uncoded-marker'>(blank)</span>
-            }
+            { this.props.localState.merge_mode ?
+              <MergeBoxHeader value_strings={value_strings} mergeData={mergeData}/> :
+              <QuestionBoxHeader number_of_sentences={number_of_sentences} value_strings={value_strings}/> }
             <hr/>
             <div className="coding-form-question-info">
               {this.props.content.info}
