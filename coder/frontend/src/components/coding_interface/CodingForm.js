9357d319c1b33ab7997ac2a5c54cbe069e1e83e3
David Benjamin Stein <davidbstein@gmail.com>
2020-07-02 04:28:21 -0400

    breakout is close...

--- coder/frontend/src/components/coding_interface/CodingForm.js
+++ coder/frontend/src/components/coding_interface/CodingForm.js
@@ -46,7 +46,7 @@ const MultiselectActiveArea = connect(
             <MergeTool
               question_idx={this.props.idx}
               question_identifier={this.props.content.identifier}
-              mergeData={mergeData} /> : <div />
+              mergeData={this.props.mergeData} /> : <div />
             }
         <div className="coding-form-sentence-list">
           {stringifySentences(this.props.sentences).map((s, i) => (
@@ -139,6 +139,7 @@ const QuestionBox = connect(
         is_active={is_active}
         sentences={sentences}
         cur_question={cur_question}
+        mergeData={mergeData}
       />
 
       return <div className="coding-form-question-container">
@@ -179,23 +180,39 @@ const BreakoutHeader = connect(
       window.SESSION_TIMER.run_timer(this.props.content.identifier);
     }
 
-    getMergeData() {
-      const to_ret = {responses: [], authors: []}
-      for (var ci of _.values(this.props.model.coding_instances)){
-        if (ci.coding_values[this.props.idx]){
-          to_ret.responses.push(ci.coding_values[this.props.idx]);
-          to_ret.authors.push(ci.coder_email);
-        }
-      }
-      const sentences_by_coder = {}
-      for (var ci of _.values(this.props.model.coding_instances)){
-        const sentences = ((ci.coding_values[this.props.idx] || {}).sentences || {})[this.props.policy_type];
-      }
-      return to_ret
-    }
-
     render() {
-      return "HEADER"
+      const cur_question = (this.props.localState.localCoding[this.props.content.identifier] || this.props.localState.localCoding[this.props.idx] || {});
+      const sentences = (cur_question.sentences||{});
+      const number_of_sentences = sentenceCount(sentences);
+      const is_active = this.props.content.identifier.startsWith(this.props.localState.selectedQuestionIdentifier.split("(")[0]);
+      const cur_values = cur_question.values || {};
+      const cur_confidence = cur_question.confidence || "unspecified";
+      const value_strings = _.keys(cur_values)
+        .filter((k) => cur_values[k])
+        .map((k) => k === "OTHER" ? `OTHER:${cur_values[k]}` : k);
+      const classes = "coding-form-question " + (is_active ? "active-question" : "inactive-question")
+
+      const active_area = <MultiselectActiveArea
+        content={this.props.content}
+        idx={this.props.idx}
+        is_active={is_active}
+        sentences={sentences}
+        cur_question={cur_question}
+      />
+
+      return <div className="coding-form-breakout-master-container">
+        <div className={classes} onClick={is_active ? null : this.handleClick}>
+          <div className="coding-form-question-title">
+            {this.props.count}. {this.props.content.question}
+          </div>
+          <div className="coding-form-question-sentence-count">
+            <hr/>
+            <div className="coding-form-question-info">
+              {this.props.content.info}
+            </div>
+          </div>
+        </div>
+      </div>
     }
   }
 )
@@ -232,7 +249,41 @@ const BreakoutOption = connect(
     }
 
     render() {
-      return "OPTION";
+      const mergeData = this.getMergeData()
+      const cur_question = (this.props.localState.localCoding[this.props.content.identifier] || this.props.localState.localCoding[this.props.idx] || {});
+      const sentences = (cur_question.sentences||{});
+      const number_of_sentences = sentenceCount(sentences);
+      const is_active = this.props.content.identifier == this.props.localState.selectedQuestionIdentifier;
+      const is_expanded = this.props.content.identifier.startsWith(this.props.localState.selectedQuestionIdentifier.split("(")[0]);
+      const cur_values = cur_question.values || {};
+      const cur_confidence = cur_question.confidence || "unspecified";
+      const value_strings = _.keys(cur_values)
+        .filter((k) => cur_values[k])
+        .map((k) => k === "OTHER" ? `OTHER:${cur_values[k]}` : k);
+      const classes = "coding-form-question " + (is_expanded ? "active-question" : "inactive-question")
+
+      const active_area = <MultiselectActiveArea
+        content={this.props.content}
+        idx={this.props.idx}
+        is_active={is_active}
+        sentences={sentences}
+        cur_question={cur_question}
+      />
+
+      return <div className="coding-form-breakout-option-container">
+        <div className={classes} onClick={is_active ? null : this.handleClick}>
+          <div className="coding-form-question-sentence-count">
+            { this.props.localState.merge_mode ?
+              <MergeBoxHeader value_strings={value_strings} mergeData={mergeData}/> :
+              <QuestionBoxHeader number_of_sentences={number_of_sentences} value_strings={value_strings} confidence={cur_confidence}/> }
+            <hr/>
+            <div className="coding-form-question-info">
+              {this.props.content.info}
+            </div>
+          </div>
+          {is_active ? active_area : <div className="inactive-selection-area"/>}
+        </div>
+      </div>
     }
   }
 )
