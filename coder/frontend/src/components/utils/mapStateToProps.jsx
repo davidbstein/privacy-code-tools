fa33d3d25b2f7eb4dfff8a3412f1c3bd1bd4f0ad
David Stein <davidbstein@gmail.com>
2021-05-23 07:05:42 -0400

    adding a coding editor. wasting time on typesciprt

--- coder/frontend/src/components/utils/mapStateToProps.jsx
+++ coder/frontend/src/components/utils/mapStateToProps.jsx
@@ -1,12 +1,13 @@
-import { useParams } from "react-router-dom";
-
-export default (
-    (state) => ({
-        model: state.model,
-        codingProgress: state.codingProgressStore,
-        localState: state.localState,
-        route: useParams()
-    })
-);
-  
-  
+/**
+ * @param state
+ * @param {ModelState} state.model
+ * @param {CodingProgressStateObject} state.codingProgress
+ * @param {LocalState} state.localState
+ * @param {Function | Null} useParams
+ */
+export default (state, useParams = null) => ({
+  model: state.model,
+  codingProgress: state.codingProgressStore,
+  localState: state.localState,
+  route: useParams ? useParams : null,
+});
